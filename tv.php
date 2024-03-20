c062a8822b273c072e71d32cd3c300ef34e15818
DoraemonX <75873265+doraemon99-x@users.noreply.github.com>
2024-03-21 02:47:49 +0700

    Update tv.php

--- tv.php
+++ tv.php
@@ -1,10 +1,10 @@
 <?php
 $url = $_GET['file'];
-$filename = basename($_SERVER['REQUEST_URI'], '? . $_SERVER['QUERY_STRING']);
+$filename = basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']);
 $urlpath = str_replace($filename, '', $url);
 
 $header = [
-    'User-Agent: Dalvik/2.1.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0", 
+    'User-Agent: Dalvik/2.1.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0', 
     'Referer: https://www.visionplus.id/'
     ];
 
@@ -15,7 +15,7 @@ $anu  = curl_exec($ch);
 curl_close($ch);
 header('content-type: application/dash+xml');
 
-$anu = str_replace('initialization="", "initialization=\"https://gvp.tipiku.site/nonton.php?file={$urlpath}", $anu);
-$anu = str_replace('media=", "media=\"https://gvp.tipiku.site/nonton.php?file={$urlpath}", $anu); 
+$anu = str_replace('initialization="', "initialization=\"https://gvp.tipiku.site/nonton.php?file={$urlpath}", $anu);
+$anu = str_replace('media="', "media=\"https://gvp.tipiku.site/nonton.php?file={$urlpath}", $anu); 
 echo $anu;
 ?>
