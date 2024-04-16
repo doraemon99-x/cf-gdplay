65e153012aa33df22c424435585b667bafa66ed1
DoraemonX <75873265+doraemon99-x@users.noreply.github.com>
2024-04-16 19:10:02 +0700

    Update tv.php

--- tv.php
+++ tv.php
@@ -5,7 +5,7 @@ $urlpath = str_replace($filename, '', $url);
 
 $header = [
     'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
-    'Referrer: https://shoof.alkass.net'
+    'Referrer: https://visionplus.id/'
     ];
 
 $ch = curl_init($url);
@@ -13,7 +13,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
 $anu  = curl_exec($ch);
 curl_close($ch);
-header('content-type: application/x-mpegURL');
+header('content-type: application/dash+xml');
 
 $anu = str_replace('initialization="', "initialization=\"https://gvp.tipiku.site/tv.php?file={$urlpath}", $anu);
 $anu = str_replace('media="', "media=\"https://gvp.tipiku.site/tv.php?file={$urlpath}", $anu); 
